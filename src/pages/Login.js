79b7f331ec5f1cf9be7beb1d79b5c78b5fcc77af
Gabor Szita <gaborszita88@gmail.com>
2023-06-11 20:15:47 +0100

    use <> instead of <section>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Login = ({userState}) => {
   return (
-    <section>
-    <p>Login page</p>
-    {userState.loggedIn ? (
-        <p>loggedin</p>
-      ) : (
-        <p>not loggedin</p>
-      )}
-    </section>
+    <>
+      <p>Login page</p>
+      {userState.loggedIn ? (
+          <p>loggedin</p>
+        ) : (
+          <p>not loggedin</p>
+        )}
+    </>
   )
 };
 
