1e9a2a62964609085b673c37873e54be2b6ad4b1
Gabor Szita <gaborszita88@gmail.com>
2023-06-11 20:16:00 +0100

    start working on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
+    <>
     <p>Home page</p>
+    <ul>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </ul>
+    </>
   )
 };
 
