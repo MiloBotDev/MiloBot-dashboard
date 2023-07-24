9cc4f358385f0cda7501206860d1f3508312ea88
Gabor Szita <gaborszita88@gmail.com>
2023-07-24 14:15:44 +0200

    add basic dashboard sidebar

--- src/pages/dashboard/guild/GuildDashboard.js
+++ src/pages/dashboard/guild/GuildDashboard.js
@@ -1,13 +1,28 @@
 import styles from './GuildDashboard.module.scss';
 import NavBar from '../../../components/NavBar';
 import { EnsureLogin } from '../../../utils/Login';
+import { Speedometer2, ListColumns } from 'react-bootstrap-icons';
 
 const Guild = ({userState, setUserState}) => {
   return (
     <>
       <EnsureLogin userState={userState} />
       <NavBar userState={userState} setUserState={setUserState} />
-       <h1 className={styles['main-text-fade-in']}>Coming soon!</h1>
+      {/* Siebar source: https://getbootstrap.com/docs/5.0/examples/sidebars/ */}
+      <div className='d-flex flex-column flex-shrink-0 p-3 text-white bg-dark' style={{ width: '280px' }}>
+        <ul className='nav nav-pills flex-column mb-auto'>
+          <li className='nav-item'>
+            <a href="/" className='nav-link active' aria-current='page'>
+              <Speedometer2 className={'me-2 ' + styles['bi']} /> Dashboard
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href="/" className='nav-link text-white'>
+              <ListColumns className={'me-2 ' + styles['bi']} /> Commands
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   )
 };
